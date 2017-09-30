1ed5d7704a91a1b27853632e22e647947e102f8e
XiaoHang <1083594261@1-PC>
2017-09-30 17:44:01 +0800

    挖坑--状态设计模式

--- Test/com/DynamicProxy/test/Save.java
+++
@@ -1,7 +0,0 @@
-package com.DynamicProxy.test;
-/*
- * 被代理类的接口
- */
-public interface Save {
-	public void Save_1();
-}
---
+++ Test/com/statepattern/olddesign/vo/CeilingFan.java
@@ -0,0 +1,9 @@
+package com.statepattern.olddesign.vo;
+/**
+ * 迪奥神
+ * @author Administrator
+ *
+ */
+public class CeilingFan {
+
+}
