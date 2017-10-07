6da8a1a4e22a163c1cfd89b6b6c68a20079cb6d8
XiaoHang <1083594261@1-PC>
2017-10-07 10:09:38 +0800

    去掉不需要的代码

--- src/test/java/com/statepattern/newdesign/vo/CeilingFanTest.java
+++ src/test/java/com/statepattern/newdesign/vo/CeilingFanTest.java
@@ -7,10 +7,6 @@ public class CeilingFanTest {
 	@Test
 	public void testPull() throws InterruptedException{
 		CeilingFan fan = new CeilingFan();
-		 int x = 10 ;
-	        double y = 20.2 ;
-	        long z = 10L; 
-		String str = "" +( x + y * z) ;
 		while(true){
 			Thread.sleep(1000L);
 			fan.pull();
